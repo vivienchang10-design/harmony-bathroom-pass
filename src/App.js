abc312f80fd6608e70ca94e7ef271520ed9f3bbf
vivianchang <vivian.chang10@gmail.com>
2025-07-31 18:52:02 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Harmony Bathroom Pass App</h1>
+      <p>This app tracks student bathroom usage. (React + Firebase)</p>
+    </div>
+  );
+}
+
+export default App;
